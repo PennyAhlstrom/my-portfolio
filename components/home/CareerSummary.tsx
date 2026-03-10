edb9e3bf7544f992c311ca076771c38af452fb7b
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-10 19:07:06 -0400

    updated career summary to use text ui component instead of plain html li items

--- components/home/CareerSummary.tsx
+++ components/home/CareerSummary.tsx
@@ -1,39 +1,40 @@
 import Section from "@/components/ui/SectionWrapper";
 import Container from "@/components/layout/Container";
 import SectionHeader from "@/components/ui/SectionHeader";
+import Text from "@/components/ui/Text";
 
 export default function CareerSummary() {
   return (
-    <Section className="py-16 bg-gray-50">
+    <Section className="bg-gray-50">
       <Container>
-        <SectionHeader>Career Summary</SectionHeader>
+        <SectionHeader variant="dark">Career Summary</SectionHeader>
         {/* <h2 className="text-2xl font-semibold">Career Summary</h2> */}
 
-        <ul className="mt-6 space-y-3 text-gray-700">
-          <li>
+         <ul className="mt-6 space-y-3 list-disc pl-6">
+          <Text as="li" variant="dark">
             Reduced projected API costs by 82% by replacing paid services with
             open-source alternatives.
-          </li>
+          </Text>
 
-          <li>
+          <Text as="li" variant="dark">
             Built machine learning models achieving over 90% test accuracy on
             classification tasks.
-          </li>
+          </Text>
 
-          <li>
+          <Text as="li" variant="dark">
             Developed full-stack applications using the MERN stack supporting
             CRUD operations and authentication.
-          </li>
+          </Text>
 
-          <li>
+          <Text as="li" variant="dark">
             Designed user-friendly interfaces that improved workflow efficiency
             for project users.
-          </li>
+          </Text>
 
-          <li>
+          <Text as="li" variant="dark">
             Created AI-driven projects integrating Python, TensorFlow, and data
             analysis tools.
-          </li>
+          </Text>
         </ul>
       </Container>
     </Section>
