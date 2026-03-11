b535c58ffe25893a75abc7b3c8b792a4d6f7bb7e
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-10 23:09:39 -0400

    removed html element p from contact section

--- components/home/ContactSection.tsx
+++ components/home/ContactSection.tsx
@@ -4,6 +4,7 @@ import SectionHeader from "@/components/ui/SectionHeader";
 //import Link from "next/link";
 import Text from "@/components/ui/Text";
 import Button from "@/components/ui/Button";
+import AppLink from "@/components/ui/AppLink";
 
 export default function ContactSection() {
   return (
@@ -12,21 +13,16 @@ export default function ContactSection() {
         <SectionHeader variant="dark">Get in Touch</SectionHeader>
         {/* <h2 className="text-2xl font-semibold">Get in Touch</h2> */}
 
-        <p className="mt-4 max-w-xl text-gray-700">
-          If you're interested in collaborating, discussing a project, or learning
-          more about my work, I would be happy to connect.
-        </p>
-
         <Text className="mt-4 max-w-xl">
           If you're interested in collaborating, discussing a project, or
           learning more about my work, I would be happy to connect.
         </Text>
 
         <div className="mt-6">
-          <Button href="/contact" theme="light">
-            Contact Me
-          </Button>
+          <Button href="/contact" theme="light">Contact Me</Button>
+
         </div>
+
       </Container>
     </Section>
   );
