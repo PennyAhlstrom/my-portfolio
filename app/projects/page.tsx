a8b2bacfb96b721b27a67f18d34cac7961d82913
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-16 19:42:44 -0400

    simplify products page to render a single combined section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,9 @@
 import ProjectsIntro from "@/components/projects/ProjectsIntro";
-import ProjectGrid from "@/components/projects/ProjectGrid";
 
 export default function ProjectsPage() {
   return (
     <>
       <ProjectsIntro />
-      <ProjectGrid />
     </>
   );
 }
