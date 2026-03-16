ba9c44c4dfe4427455948d2201179407942a99ef
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-16 19:34:47 -0400

    move demo grid into intro section for combined demos layout

--- components/demos/DemosIntro.tsx
+++ components/demos/DemosIntro.tsx
@@ -2,16 +2,18 @@ import Section from "@/components/ui/SectionWrapper";
 import Container from "@/components/layout/Container";
 import SectionHeader from "@/components/ui/SectionHeader";
 import Text from "@/components/ui/Text";
+import DemoGrid from "@/components/demos/DemoGrid";
 
 export default function DemosIntro() {
   return (
-    <Section>
+    <Section variant="plain">
       <Container>
         <SectionHeader>Demos</SectionHeader>
 
         <Text className="mt-4 max-w-3xl">
           This page highlights video demonstrations of selected projects and technical work.
         </Text>
+        <DemoGrid />
       </Container>
     </Section>
   );
