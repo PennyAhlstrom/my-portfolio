188d6b8105061e22c8d4dc30e171b916311f9df6
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-16 19:41:33 -0400

    remove section wrapper from project grid for unified page layout

--- components/projects/ProjectGrid.tsx
+++ components/projects/ProjectGrid.tsx
@@ -1,13 +1,9 @@
-import Section from "@/components/ui/SectionWrapper";
-import Container from "@/components/layout/Container";
 import ProjectCard from "@/components/projects/ProjectCard";
 import { projects } from "@/components/projects/ProjectData";
 
 export default function ProjectGrid() {
   return (
-    <Section className="bg-gray-50">
-      <Container>
-        <div className="grid gap-8 md:grid-cols-2">
+    <div className="mt-10 grid gap-y-10 gap-x-8 md:grid-cols-2">
            {projects.map((project) => (
             <ProjectCard
               key={project.slug}
@@ -18,7 +14,5 @@ export default function ProjectGrid() {
             />
           ))}
         </div>
-      </Container>
-    </Section>
   );
 }
