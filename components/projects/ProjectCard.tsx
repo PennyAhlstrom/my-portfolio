42d00da79fa5930333109ab971cbe9eb91fad300
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-16 18:49:16 -0400

    add tech icon mapping to tech badges

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import Card from "@/components/ui/Card";
 import Text from "@/components/ui/Text";
 import TechBadge from "@/components/tech/TechBadge";
+import { techIcons } from "@/components/tech/techIcons";
 
 type ProjectCardProps = {
   title: string;
@@ -20,9 +21,18 @@ export default function ProjectCard({
       <Text className="text-muted">{description}</Text>
 
       <div className="mt-5 flex flex-wrap gap-2">
-        {tech.map((item) => (
-          <TechBadge key={item}>{item}</TechBadge>
-        ))}
+        {tech.map((item) => {
+          const Icon = techIcons[item]
+
+          return (
+            <TechBadge
+              key={item}
+              icon={Icon ? <Icon size={12} /> : undefined}
+            >
+              {item}
+            </TechBadge>
+          )
+        })}
       </div>
     </Card>
   );
