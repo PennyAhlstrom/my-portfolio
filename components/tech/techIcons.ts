4b75efe2f8834ee378489dbe0f9fcb007cb70628
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-16 18:45:38 -0400

    create icon map for tech names

---
+++ components/tech/techIcons.ts
@@ -0,0 +1,23 @@
+import {
+  SiReact,
+  SiNextdotjs,
+  SiTypescript,
+  SiTailwindcss,
+  SiNodedotjs,
+  SiMongodb,
+  SiPython,
+  SiTensorflow,
+} from "react-icons/si"
+
+import { IconType } from "react-icons"
+
+export const techIcons: Record<string, IconType> = {
+  React: SiReact,
+  "Next.js": SiNextdotjs,
+  TypeScript: SiTypescript,
+  Tailwind: SiTailwindcss,
+  Node: SiNodedotjs,
+  MongoDB: SiMongodb,
+  Python: SiPython,
+  TensorFlow: SiTensorflow,
+}
