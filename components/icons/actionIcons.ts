01f0a24419b2d6bacb2d61171fe6a2d8620dd98c
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-16 20:43:58 -0400

    centralize action icons into shared icon mapping

---
+++ components/icons/actionIcons.ts
@@ -0,0 +1,9 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+import { MdEmail } from "react-icons/md"
+import { IconType } from "react-icons"
+
+export const actionIcons: Record<string, IconType> = {
+  github: FaGithub,
+  linkedin: FaLinkedin,
+  email: MdEmail,
+}
